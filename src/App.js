88368724ed7553e7a6eae853c66492526639f5e4
Wahyu Febrianto <febriantowahyu63@gmail.com>
2020-05-10 00:53:47 +0700

    Update Files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
-function App() {
+import Home from "./components/home/Home";
+import Tickets from "./components/tickets/Tickets";
+import DetailFilm from "./components/detail-film/DetailFilm";
+import Registration from "./components/auth/registration/Registration";
+import Login from "./components/auth/login/Login";
+
+import { Route, Switch  } from "react-router-dom";
+import {MobileView} from "react-device-detect"
+import {useMediaQuery} from "react-responsive";
+
+const App =()=>{
+  const isMobile = useMediaQuery({query:"(max-width:425px)"});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isMobile && (
+        <MobileView>
+          <Switch>
+            <Route path="/" component={Home} exact></Route>
+            <Route path="/registration" component={Registration}></Route>
+            <Route path="/login" component={Login}></Route>
+            <Route path="/tickets" component={Tickets}></Route>
+            <Route path="/detail-film/:id" component={DetailFilm}></Route>
+          </Switch>
+      </MobileView>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css"; 
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+import { BrowserRouter } from "react-router-dom";
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
